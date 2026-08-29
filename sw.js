343bbc7d14c00e8dfac3c378a0da69b281edd80c
fzldmarcos-source <fzld.marcos@gmail.com>
2026-08-29 09:43:22 -0400

    Sincronizar automáticamente al recuperar conexión
    Combina cambios de ambos equipos y actualiza el almacenamiento local para uso sin conexión.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fitquest-v3.2-sync';
+const CACHE='fitquest-v3.3-sync';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
